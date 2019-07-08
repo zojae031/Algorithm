20982bb9e205156a910363a5c79a97a1fae935f1
Zojae031 <edgar3@naver.com>
2019-07-08 18:47:40 +0900

    모두 같은 높이를 가질 경우 구해야함

--- Zojae031/CodingTest/2304.cpp
+++ Zojae031/CodingTest/2304.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
 #include<stack>
 using namespace std;
-
+int findArea(int *area, stack<pair<int, int>> *s) {
+	while (true) {
+		pair<int, int> data = s->top();
+		s->pop();
+		if (s->empty())break;
+		int width = abs(data.second - s->top().second);
+		int height = s->top().first;
+		*area += width * height;
+	}
+	return *area;
+}
 int main() {
 	int **arr; //y ,x
 
@@ -9,12 +19,12 @@ int main() {
 	int num, maxHeight = 0, maxLen = 0;
 	int maxHeightIndex = 0;
 	int x, y;
-	int area=0;
+	int area = 0;
 	int xCnt = 0, yCnt = 0;
 	cin >> num;
-	
-	
-	
+
+
+
 	for (int i = 0; i < 2; i++)
 		buf[i] = (int*)calloc(num, sizeof(int));
 
@@ -30,9 +40,9 @@ int main() {
 		buf[0][i] = x;
 		buf[1][i] = y;
 	}
-	arr = (int**)calloc(maxHeight+1, sizeof(int*));
+	arr = (int**)calloc(maxHeight + 1, sizeof(int*));
 
-	for (int i = 0; i < maxHeight+1; i++) {
+	for (int i = 0; i < maxHeight + 1; i++) {
 		arr[i] = (int*)calloc(maxLen, sizeof(int));
 	}
 
@@ -43,47 +53,47 @@ int main() {
 		}
 	}
 
-	for (int i = 0; i < maxHeight+1; i++) {//y축
-		for (int j = 0; j < maxLen; j++) { //x축
-			cout << arr[i][j];
-		}
-		cout << endl;
-	}
 
-
-	stack<pair<int,int>> s;
+	stack<pair<int, int>> s;
 	s.push(pair<int, int>(0, 0));
 	int i = 0;
 	int j = 0;
-	
-		while(j<=maxHeightIndex &&i<=maxHeight){
-			if (arr[i][j] == 0) {
-				
-				pair<int, int>compare = s.top();
-				if (i > compare.first) { //현재 높이가 스택에 있는애보다 크면 푸쉬
-					s.push(pair<int,int>(i,j));
-				}
-				j++;
+
+	while (j <= maxHeightIndex && i <= maxHeight) {
+		if (arr[i][j] == 0) {
+			pair<int, int>compare = s.top();
+			if (i > compare.first) { //현재 높이가 스택에 있는애보다 크면 푸쉬
+				s.push(pair<int, int>(i, j));
 			}
-			else {
-				i++;
+			j++;
+		}
+		else {
+			i++;
+		}
+	}
+
+
+	area = findArea(&area, &s);
+
+	s.push(pair<int, int>(0, 0));
+	j = maxLen - 1;
+	i = 0;
+	while (j >= maxHeightIndex-1 && i <= maxHeight) {
+		if (arr[i][j] == 0) {
+			pair<int, int>compare = s.top();
+			if (i > compare.first) { //현재 높이가 스택에 있는애보다 크면 푸쉬
+				s.push(pair<int, int>(i, j));
 			}
-
+			j--;
 		}
-	
-		while (true) {
-			pair<int, int> data = s.top();
-			s.pop();
-			if (s.empty())break;
-			int width = data.second - s.top().second;
-			int height = s.top().first;
-			area += width * height;
-
+		else {
+			i++;
 		}
+	}
 
-		cout << area;
-
-
+	area = findArea(&area, &s);
+	area += maxHeight;
+	cout << area;
 
 	return 0;
 }
